1996db03bd44a4e89fd148fabc320f64335abaa6
Gboyega Dada <boyega@gmail.com>
2020-08-28 21:29:05 +0100

    Fix js event subscriber

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NativeEventEmitter, NativeModules } from 'react-native'
-import { EventEmitter, EventSubscription } from 'fbemitter'
-import { ProtectedDataEvent, ProtectedDataJSEventName, ProtectedDataListener } from './ProtectedData';
+import { EventSubscription } from 'fbemitter'
+import { ProtectedDataJSEventName, ProtectedDataListener } from './ProtectedData';
 
 export * from './ProtectedData.d'
 
@@ -9,33 +9,19 @@ console.log(React.version)
 
 // Initialize the native event emitter
 const nativeEventEmitter = new NativeEventEmitter(NativeModules.ProtectedDataIOS);
-nativeEventEmitter.addListener('ApplicationProtectedDataEvent', (e: ProtectedDataEvent) => ProtectedDataIOS.emit(e))
-
-// Initialize the user-facing event emitter
-const eventEmitter = new EventEmitter()
 
 export class ProtectedDataIOS {
-  
-  /**
-   * Relay native event to JS listeners
-  */
- static emit(e: ProtectedDataEvent): void {
-  
-  switch(e.name) {
-    case "UIApplicationProtectedDataDidBecomeAvailable":
-      eventEmitter.emit('didBecomeAvailable', e)
-      break
-    case "UIApplicationProtectedDataWillBecomeUnavailable":
-      eventEmitter.emit('willBecomeUnavailable', e)
-      break
+
+  static mapped = {
+    willBecomeUnavailable: "UIApplicationProtectedDataWillBecomeUnavailable",
+    didBecomeAvailable: "UIApplicationProtectedDataDidBecomeAvailable",
   }
- }
 
  /**
   * Add an event handler that is fired when native event is received.
   */
  static addListener(name: ProtectedDataJSEventName, listener: ProtectedDataListener): EventSubscription {
-   return eventEmitter.addListener(name, listener);
+   return nativeEventEmitter.addListener(this.mapped[name], listener)
  }
 
 }
